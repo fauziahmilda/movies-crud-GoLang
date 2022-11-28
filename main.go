e0a770d3b836bcecc2a350189275c5efe1b832b5
fauziahmilda <fauziah.milda@gmail.com>
2022-11-28 18:13:03 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	//stuff
+	"fmt"
+	//log any errors
+	"log"
+	//encode my data into json when send to postman
+
+	//new id by this method
+
+	//server
+	"net/http"
+	//id will convert to string
+
+	//package github, gorilla
+	"github.com/gorilla/mux"
+)
+
+type Movie struct {
+	ID       string    `json:"id"`
+	Isbn     string    `json:"isbn"`
+	Title    string    `json: "title"`
+	Director *Director `json: "director"`
+}
+
+type Director struct {
+	Firstname string `json: "firstname"`
+	Lastname  string `json: "lastname"`
+}
+
+var movies []Movie
+
+func main() {
+	//this is from mux liblary
+	r := mux.NewRouter()
+
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+
+	fmt.Printf("Starting server at port 8080\n")
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
